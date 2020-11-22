f31d5270c882a12eb7acbca15ee9fbbeffffaaae
Anurag Modi <modi.anurag1992@gmail.com>
2020-11-22 15:20:14 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source activate ta
 cd /home/anurag/ta_using_TA-Lib
+git clean -dffx
+git reset --hard HEAD~5
+git pull
 /home/anurag/anaconda3/envs/ta/bin/python stock_quotes.py > stocks_analyze.txt
 sleep 5m
 python send_mail.py
