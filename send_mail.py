760fe117ef7dc736ac2a6549b2afe32168c81e25
Anurag Modi <modi.anurag1992@gmail.com>
2023-07-03 00:02:57 +0530

    Upgrade some code with pre-commit

--- send_mail.py
+++ send_mail.py
@@ -1,43 +1,52 @@
+import datetime
 import os
 import smtplib
-import datetime
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase 
 from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 sender_email_id = "modi.anurag1992@gmail.com"
-sender_email_pwd = "Sainik@3755"
-recipients_email_id = [
+sender_email_pwd = ""
+recipients_email_id: list[str] = [
     "modi.anurag1992@gmail.com",
-    "poojamangal.imtn@gmail.com",
-    "agrawaldrnitin@gmail.com",
 ]
-date    = datetime.datetime.now().strftime("%d-%m-%Y")
-subject = f"picks_of_the_day :- {date}"
+date: str = datetime.datetime.now().strftime("%d-%m-%Y")
+subject: str = f"picks_of_the_day :- {date}"
 message = "Analyze_before_investing"
 attachment_path = "stocks_analyze.txt"
 
-def sendEmail(sender_email_id, pwd, recipsEmailID, subject, message, attachment_path):
+
+def sendEmail(
+    sender_email_id, pwd, recipsEmailID, subject, message, attachment_path
+) -> None:
     msg = MIMEMultipart()
-    msg['From'] = sender_email_id
-    recips = recipsEmailID
-    msg['To'] = ",".join(recips)
+    msg["From"] = sender_email_id
+    recips: list = recipsEmailID
+    msg["To"] = ",".join(recips)
     msg["Subject"] = subject
-    msg.attach(MIMEText(message, 'plain'))
+    msg.attach(MIMEText(message, "plain"))
     # open the file to be sent
-    filename = os.path.basename(attachment_path)
-    attachment = open(attachment_path, "rb") 
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload((attachment).read()) 
+    filename: str = os.path.basename(attachment_path)
+    attachment = open(attachment_path, "rb")
+    p = MIMEBase("application", "octet-stream")
+    p.set_payload((attachment).read())
     encoders.encode_base64(p)
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    p.add_header("Content-Disposition", "attachment; filename= %s" % filename)
     msg.attach(p)
 
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s = smtplib.SMTP(host="smtp.gmail.com", port=587)
     s.starttls()
-    s.login(sender_email_id, pwd) 
+    s.login(sender_email_id, pwd)
     s.sendmail(sender_email_id, recips, msg.as_string())
     s.quit()
 
-sendEmail(sender_email_id, sender_email_pwd, recipients_email_id, subject, message, attachment_path)
+
+sendEmail(
+    sender_email_id,
+    sender_email_pwd,
+    recipients_email_id,
+    subject,
+    message,
+    attachment_path,
+)
--- utils.py
+++ utils.py
@@ -1,81 +1,110 @@
-
+import pandas as pd
 import talib
 
-def is_consolidating(df, percentage=2.5):
-    recent_candlesticks = df[-15:]
 
-    max_close = recent_candlesticks['Close'].max()
-    min_close = recent_candlesticks['Close'].min()
+def is_consolidating(df: pd.DataFrame, percentage=2.5) -> bool:
+    """The function `is_consolidating` checks if the recent candlesticks in a.
 
-    threshold = 1 - (percentage / 100)
+    DataFrame are consolidating based on a given percentage threshold.
+
+    Args:
+        df (pd.DataFrame): A pandas DataFrame containing the historical price
+        data of a financial instrument percentage (float, optional):
+        The `percentage` parameter is the threshold percentage used to determine
+        if the price is consolidating. Defaults to 2.5.
+
+    Returns:
+        bool: a boolean value. It returns True if the minimum closing price in
+        the recent candlesticks is greater than the maximum closing price
+        multiplied by the threshold, and False otherwise.
+    """
+
+    recent_candlesticks: pd.DataFrame = df[-15:]
+
+    max_close: pd.Series = recent_candlesticks["Close"].max()
+    min_close: pd.Series = recent_candlesticks["Close"].min()
+
+    threshold: float = 1 - (percentage / 100)
     if min_close > (max_close * threshold):
         return True
 
     return False
 
+
 def is_breaking_out(df, percentage=2.5):
-    last_close = df[-1:]['Close'].values[0]
+    last_close = df[-1:]["Close"].values[0]
 
     if is_consolidating(df[:-1], percentage=percentage):
         recent_closes = df[-16:-1]
 
-        if last_close > recent_closes['Close'].max():
+        if last_close > recent_closes["Close"].max():
             return True
 
     return False
 
 
-## Implement TTM SQUEEZE
+# Implement TTM SQUEEZE
 def ttm_squeeze(df):
-    df['20sma'] = df['Close'].rolling(window=20).mean()
-    df['stddev'] = df['Close'].rolling(window=20).std()
-    df['lower_band'] = df['20sma'] - (2 * df['stddev'])
-    df['upper_band'] = df['20sma'] + (2 * df['stddev'])
+    df["20sma"] = df["Close"].rolling(window=20).mean()
+    df["stddev"] = df["Close"].rolling(window=20).std()
+    df["lower_band"] = df["20sma"] - (2 * df["stddev"])
+    df["upper_band"] = df["20sma"] + (2 * df["stddev"])
 
-    df['TR'] = abs(df['High'] - df['Low'])
-    df['ATR'] = df['TR'].rolling(window=20).mean()
+    df["TR"] = abs(df["High"] - df["Low"])
+    df["ATR"] = df["TR"].rolling(window=20).mean()
 
-    df['lower_keltner'] = df['20sma'] - (df['ATR'] * 1.5)
-    df['upper_keltner'] = df['20sma'] + (df['ATR'] * 1.5)
+    df["lower_keltner"] = df["20sma"] - (df["ATR"] * 1.5)
+    df["upper_keltner"] = df["20sma"] + (df["ATR"] * 1.5)
 
     def in_squeeze(df):
-        return df['lower_band'] > df['lower_keltner'] and df['upper_band'] < df['upper_keltner']
+        return (
+            df["lower_band"] > df["lower_keltner"]
+            and df["upper_band"] < df["upper_keltner"]
+        )
 
-    df['squeeze_on'] = df.apply(in_squeeze, axis=1)
+    df["squeeze_on"] = df.apply(in_squeeze, axis=1)
 
-    if df.iloc[-3]['squeeze_on'] and df.iloc[-1]['squeeze_on']:
+    if df.iloc[-3]["squeeze_on"] and df.iloc[-1]["squeeze_on"]:
         return True, False
-    elif df.iloc[-3]['squeeze_on'] and not df.iloc[-1]['squeeze_on']:
+    elif df.iloc[-3]["squeeze_on"] and not df.iloc[-1]["squeeze_on"]:
         return True, True
     else:
         return False, False
 
 
-## Implement ATR
-def cal_atr(df, timeperiod=14):
-    atr = talib.ATR(df['High'], df['Low'], df['Close'], timeperiod=timeperiod)
+# Implement ATR
+def cal_atr(df, timeperiod=14) -> float:
+    atr: float = talib.ATR(
+        df["High"], df["Low"], df["Close"], timeperiod=timeperiod
+    )
     return atr
 
-## Implement Super trend Indicator
+
+# Implement Super trend Indicator
 def super_trend(df, timeperiod=14, multiplier=3):
     atr = cal_atr(df, timeperiod=timeperiod)
-    df['upperband'] = ((df['High'] + df['Low']) / 2 ) + (multiplier * atr)
-    df['lowerband'] = ((df['High'] + df['Low']) / 2 ) - (multiplier * atr)
-    df['in_uptrend'] = True
+    df["upperband"] = ((df["High"] + df["Low"]) / 2) + (multiplier * atr)
+    df["lowerband"] = ((df["High"] + df["Low"]) / 2) - (multiplier * atr)
+    df["in_uptrend"] = True
 
     for current in range(1, len(df.index)):
         previous = current - 1
 
-        if df['Close'][current] > df['upperband'][previous]:
-            df['in_uptrend'][current] = True
-        elif df['Close'][current] < df['lowerband'][previous]:
-            df['in_uptrend'][current] = False
+        if df["Close"][current] > df["upperband"][previous]:
+            df["in_uptrend"][current] = True
+        elif df["Close"][current] < df["lowerband"][previous]:
+            df["in_uptrend"][current] = False
         else:
-            df['in_uptrend'][current] = df['in_uptrend'][previous]
-            if df['in_uptrend'][current] and df['lowerband'][current] < df['lowerband'][previous]:
-                df['lowerband'][current] = df['lowerband'][previous]
+            df["in_uptrend"][current] = df["in_uptrend"][previous]
+            if (
+                df["in_uptrend"][current]
+                and df["lowerband"][current] < df["lowerband"][previous]
+            ):
+                df["lowerband"][current] = df["lowerband"][previous]
 
-            if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
-                df['upperband'][current] = df['upperband'][previous]
-    return df.iloc[-4]['in_uptrend'], df.iloc[-1]['in_uptrend']
-
+            if (
+                not df["in_uptrend"][current]
+                and df["upperband"][current] > df["upperband"][previous]
+            ):
+                df["upperband"][current] = df["upperband"][previous]
+    return df.iloc[-4]["in_uptrend"], df.iloc[-1]["in_uptrend"]
