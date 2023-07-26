9d3b37a6e5ec9b130afb36f7971ed1d72c76c963
anurag3753 <modi.anurag1992@gmail.com>
2023-07-26 06:59:28 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ inputfile=$1
 outfile=$2
 date=$3
 
-cd /home/azureuser
+cd -
 rm -rf ta_using_TA-Lib
 git clone https://github.com/anurag3753/ta_using_TA-Lib.git
 cd ta_using_TA-Lib
-/home/azureuser/anaconda3/bin/python recommend.py $1 $3 > $2
+python recommend.py $1 $3 > $2
 python send_mail.py
 git add $2
 git commit -m "update recommendation"
